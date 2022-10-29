8ff04dfcc6cb98c6bf2a3d0227ad3ba0cbdc4b81
wuliqun <wuliqun@xinyu100.com>
2022-10-29 18:11:58 +0800

    moxui: config, add shaking to test tree_shaking locally

---
+++ shaking/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      moxui: resolve(__dirname, "../dist/moxui/es/index"),
+    },
+  },
+  build: {
+    target: ["es2020", "edge88", "firefox78", "chrome87", "safari13"],
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // 拆分代码，这个就是分包，配置完后自动按需加载
+          vue: ["vue"],
+          moxui: ["moxui"],
+        },
+      },
+    },
+  },
+});
